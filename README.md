1a97ac1ab8bd2431ee9b9c5da15e26017c10c952
Qimu Zheng <zheng.qm@163.com>
2015-06-24 21:05:32 +0800

    Change code format

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# SRSearch
+# SRSearch - A real-time source repo search application
+
+Massive refactoring needed!
+
+![](demo.gif)
--- SRsearch.js
+++ SRsearch.js
@@ -4,16 +4,12 @@ if (Meteor.isServer) {
   Meteor.publish('items', function (queryString) {
       return query(Items, queryString)
   })
-
-
 }
 
 if (Meteor.isClient) {
-  // This code only runs on the client
   Session.set('queryString', '')
   Meteor.subscribe('items', '')
-  
-  
+   
   Template.body.helpers({
     items: function () {
       return query(Items, Session.get('queryString'))
@@ -24,7 +20,6 @@ if (Meteor.isClient) {
     "keyup #search-box":_.throttle(function(event){
       Session.set('queryString', event.target.value)
       Meteor.subscribe('items', event.target.value)
-      
     }, 50)
   })
 }
@@ -34,7 +29,6 @@ function splitKeyword(str){
 }
 
 function isSpecial(str){
-
   var relation
   var result = {}
   if (str.indexOf('<') != -1){
@@ -54,15 +48,12 @@ function isSpecial(str){
     result[relation[0]] = new RegExp(relation[1], 'ig')
     return result
   } 
-    
   return null;
-  
 }
 
 function query(collections, queryString){
   var limit = 40
-  var query = splitKeyword(queryString)
-  //var orArray = []
+  var query = queryString.split(' ')
   var andArray = []
   for (var i = query.length - 1; i >= 0; i--) {
     if (query[i] == '') {
@@ -77,19 +68,9 @@ function query(collections, queryString){
     }
   }
 
-  /*
-  if(orArray.length != 0){
-    andArray.push({$or: orArray})
-  }
-  */
-  
   if (andArray.length != 0){
     return collections.find({$and: andArray}, {limit:limit})
   } else {
     return collections.find({},{limit:limit})
   }
-
 }
-
-
-
